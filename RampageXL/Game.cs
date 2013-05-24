89a7a5e38e41f2ea29d914542eae084bf6ebe76d
Alex Ferbrache <Alex.Ferbrache@gmail.com>
2013-05-24 09:57:02 -0700

    Initial commit of VS12 Project

---
+++ RampageXL/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace RampageXL
+{
+	class Game : GameWindow
+	{
+
+		public Game()
+			: base(1280, 360, GraphicsMode.Default, "RampageXL")
+		{
+			VSync = VSyncMode.Off;
+		}
+
+		static void Main()
+		{
+			using (Game game = new Game())
+			{
+				game.Run();
+			}
+		}
+	}
+}
